a50c450df66ae1ea3ac1b9c8338640cf35755d09
Renato Athaydes <renato@athaydes.com>
2022-10-21 20:08:06 +0200

    Always canonicalize absolute path.

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:path/path.dart' as p;
 
 String absPath(String path, [String? dir]) {
-  return p.isAbsolute(path)
-      ? path
-      : p.join(dir ?? Directory.current.path, path);
+  final normalPath =
+      p.isAbsolute(path) ? path : p.join(dir ?? Directory.current.path, path);
+  return p.canonicalize(normalPath);
 }
